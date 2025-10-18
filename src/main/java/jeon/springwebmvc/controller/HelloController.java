baf59b156454f88a34ed98a00eb69a5ef900c032
yeoreum <jeondaegam@naver.com>
2025-10-18 17:26:57 +0900

    mvc 형식의 controller 만들기
    - /hello-mvc

--- src/main/java/jeon/springwebmvc/controller/HelloController.java
+++ src/main/java/jeon/springwebmvc/controller/HelloController.java
@@ -3,6 +3,7 @@ package jeon.springwebmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public String Hello(Model model) {
         model.addAttribute("name", "Jeon");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String HelloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-mvc"; // view resolver가 hello-mvc.html을 찾는다!
     }
 }

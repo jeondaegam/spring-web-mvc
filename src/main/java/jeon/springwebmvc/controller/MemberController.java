31ec2cc88d796cedcb309b92728d98a5217ebfe4
yeoreum <jeondaegam@naver.com>
2025-10-24 19:31:26 +0900

    화면 컨트롤러 만들기
    - 기능에 화면을 붙여보자

--- src/main/java/jeon/springwebmvc/controller/MemberController.java
+++ src/main/java/jeon/springwebmvc/controller/MemberController.java
@@ -3,6 +3,7 @@ package jeon.springwebmvc.controller;
 import jeon.springwebmvc.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController{
@@ -13,4 +14,10 @@ public class MemberController{
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
 }

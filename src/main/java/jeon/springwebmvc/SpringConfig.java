05e158602ddedb6f38bd3b99acc5f482b9b4f335
yeoreum <jeondaegam@naver.com>
2025-10-30 10:11:28 +0900

    Jdbc Template 방식으로 변경
    - 순수 Jdbc와 동일한 환경설정을 하면 된다.
    - sql은 직접 작성해야함

--- src/main/java/jeon/springwebmvc/SpringConfig.java
+++ src/main/java/jeon/springwebmvc/SpringConfig.java
@@ -1,13 +1,23 @@
 package jeon.springwebmvc;
 
+import jeon.springwebmvc.repository.JdbcTemplateMemberRepository;
 import jeon.springwebmvc.repository.MemberRepository;
-import jeon.springwebmvc.repository.MemoryMemberRepository;
 import jeon.springwebmvc.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
---
+++ src/test/java/jeon/springwebmvc/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,73 @@
+package jeon.springwebmvc.service;
+
+import jeon.springwebmvc.domain.Member;
+import jeon.springwebmvc.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+//    @Autowired MemberRepository repository;
+
+//    @BeforeEach
+//    void beforeEach() {
+//        repository = new MemoryMemberRepository();
+//        memberService = new MemberService(repository);
+//    }
+
+
+//    @AfterEach
+//    void afterEach() {
+//        repository.clearStore();
+//    }
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("test-member");
+        // when
+        Long savedId = memberService.join(member);
+        // than
+        Member result = memberService.findOne(savedId).get();
+        assertThat(result.getName()).isEqualTo(member.getName());
+    }
+
+    @Test
+    void 중복_회원_예외처리() {
+
+        // given
+        Member member1 = new Member();
+        member1.setName("member");
+
+        Member member2 = new Member();
+        member2.setName("member");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2)); // 예외가 발생해야 한다.
+
+        // than
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+    }
+
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
